9d040f8c7ce70e1cc873d3095c37df96c2e17739
Hansika Gupta <hansikag22@gmail.com>
2026-03-20 19:24:33 +0530

    Add files via upload

---
+++ D45.c
@@ -0,0 +1,104 @@
+/*Problem Statement:
+Find the height (maximum depth) of a given binary tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+
+Output Format:
+- Print the height of the tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 -1
+
+Output:
+3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of tree node
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create new node
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// Function to find max
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+// Function to find height
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    
+    return 1 + max(left, right);
+}
+
+// Function to build tree from level order
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct TreeNode* root = createNode(arr[0]);
+
+    struct TreeNode* queue[n];
+    int front = 0, rear = 0;
+    
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* current = queue[front++];
+
+        // Left child
+        if (i < n && arr[i] != -1) {
+            current->left = createNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            current->right = createNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    printf("%d\n", maxDepth(root));
+
+    return 0;
+}
